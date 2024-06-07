7e8ece0e9a2cad55cd928573e58173338c7a6b79
jax138 <jaxcason304@gmail.com>
2024-06-07 15:48:18 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,8 @@
+const add = 10+2 
+console.log("The value added is",add)
+//output:The value added is 12
+
+const subs = 10-2 
+console.log("the subtracted value is",subs)
+//output: the subtracted value is 8
+
